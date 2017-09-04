0d20457ee4ed4aade0ad57214ab2489ba9e2271a
Renato Crivano <renato.crivano@gmail.com>
2017-09-04 16:37:44 -0300

    Alterando as classes relacionadas com marcadores processuais

---
+++ src/main/resources/br/jus/trf2/balcaovirtual/data-001.sql
@@ -0,0 +1,5 @@
+DELETE FROM `trfBalcaoVirtual`.`tx_tipo_marca_cnj_classe` WHERE `CNCL_ID` IN (202,436);
+INSERT INTO `trfBalcaoVirtual`.`tx_tipo_marca_cnj_classe` (`TIMA_ID`, `CNCL_ID`) VALUES ('1', '10943');
+INSERT INTO `trfBalcaoVirtual`.`tx_tipo_marca_cnj_classe` (`TIMA_ID`, `CNCL_ID`) VALUES ('1', '10944');
+INSERT INTO `trfBalcaoVirtual`.`tx_tipo_marca_cnj_classe` (`TIMA_ID`, `CNCL_ID`) VALUES ('1', '282');
+INSERT INTO `trfBalcaoVirtual`.`tx_tipo_marca_cnj_classe` (`TIMA_ID`, `CNCL_ID`) VALUES ('1', '283');
