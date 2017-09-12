5f49b11c6dbc9b2913e72bc78c72e40398484991
Renato Crivano <renato.crivano@gmail.com>
2017-09-12 10:19:58 -0300

    melhorando sql de marcadores

--- src/main/resources/br/jus/trf2/balcaovirtual/marcadores.sql
+++ src/main/resources/br/jus/trf2/balcaovirtual/marcadores.sql
@@ -1,9 +1,10 @@
 select 
-	tmi.*
+	tmi.timi_nm
 from 
 	tipo_marca_item tmi, 
     tx_tipo_marca_cnj_classe tx 
 where 
-	cncl_id = ?
+	tmi.timi_id = tx.timi_id 
+	and cncl_id = ?
 order by 
 	timi_nr_ordem;
