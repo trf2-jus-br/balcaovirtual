b9500e1b817f614ebd15336f569523ca8bb54795
Renato Crivano <renato.crivano@gmail.com>
2017-09-25 16:48:58 -0300

    Relacionando outras classes processuais a marcadores padronizados

---
+++ src/main/resources/br/jus/trf2/balcaovirtual/db-005.sql
@@ -0,0 +1,8 @@
+INSERT INTO `trfBalcaoVirtual`.`tx_tipo_marca_cnj_classe` (`TIMA_ID`, `CNCL_ID`) VALUES ('1', '293');
+INSERT INTO `trfBalcaoVirtual`.`tx_tipo_marca_cnj_classe` (`TIMA_ID`, `CNCL_ID`) VALUES ('1', '294');
+INSERT INTO `trfBalcaoVirtual`.`tx_tipo_marca_cnj_classe` (`TIMA_ID`, `CNCL_ID`) VALUES ('1', '295');
+INSERT INTO `trfBalcaoVirtual`.`tx_tipo_marca_cnj_classe` (`TIMA_ID`, `CNCL_ID`) VALUES ('1', '297');
+INSERT INTO `trfBalcaoVirtual`.`tx_tipo_marca_cnj_classe` (`TIMA_ID`, `CNCL_ID`) VALUES ('1', '299');
+INSERT INTO `trfBalcaoVirtual`.`tx_tipo_marca_cnj_classe` (`TIMA_ID`, `CNCL_ID`) VALUES ('1', '300');
+INSERT INTO `trfBalcaoVirtual`.`tx_tipo_marca_cnj_classe` (`TIMA_ID`, `CNCL_ID`) VALUES ('1', '302');
+INSERT INTO `trfBalcaoVirtual`.`tx_tipo_marca_cnj_classe` (`TIMA_ID`, `CNCL_ID`) VALUES ('1', '1710');
