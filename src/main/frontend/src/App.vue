b4b01a7987792684ed94d9d190889a2796db257e
Renato Crivano <renato.crivano@gmail.com>
2017-10-31 18:07:46 -0200

    Habilitando o item do menu da mesa

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -35,7 +35,7 @@
                 <li class="nav-item" v-if="!jwt.origin">
                   <router-link class="nav-link" active-class="active" :to="{name:'Lista de Avisos'}" tag="a">Intimação/Citação</router-link>
                 </li>
-                <li class="nav-item" v-if="!!jwt.origin &amp;&amp; test.properties['balcaovirtual.env'] !== 'prod'">
+                <li class="nav-item" v-if="!!jwt.origin">
                   <router-link class="nav-link" active-class="active" :to="{name:'Mesa'}" tag="a">Mesa</router-link>
                 </li>
                 <li class="nav-item">
