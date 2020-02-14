31df4f951415f072212c8d3f98ac480b3e0be637
Renato Crivano <renato.crivano@gmail.com>
2020-02-14 18:15:40 -0200

    Renomeando sistemas no banco de dados

---
+++ src/main/resources/br/jus/trf2/balcaovirtual/db-013.sql
@@ -0,0 +1,3 @@
+UPDATE `bv`.`sistema` SET `SIST_SG` = 'br.jus.trf2.apolo' WHERE (`SIST_ID` = '1');
+UPDATE `bv`.`sistema` SET `SIST_SG` = 'br.jus.jfrj.apolo' WHERE (`SIST_ID` = '2');
+UPDATE `bv`.`sistema` SET `SIST_SG` = 'br.jus.jfes.apolo' WHERE (`SIST_ID` = '3');
