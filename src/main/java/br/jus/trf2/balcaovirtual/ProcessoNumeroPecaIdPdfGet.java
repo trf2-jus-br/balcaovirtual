bf81aea76a01f7e1f645604c5ea0fb8591de3b75
Renato Crivano <renato.crivano@gmail.com>
2020-03-27 13:32:43 -0300

    Corrigindo log desnecessário de exceção

--- src/main/java/br/jus/trf2/balcaovirtual/ProcessoNumeroPecaIdPdfGet.java
+++ src/main/java/br/jus/trf2/balcaovirtual/ProcessoNumeroPecaIdPdfGet.java
@@ -1,5 +1,7 @@
 package br.jus.trf2.balcaovirtual;
 
+import com.crivano.swaggerservlet.SwaggerAuthorizationException;
+
 import br.jus.trf2.balcaovirtual.AutenticarPost.Usuario;
 import br.jus.trf2.balcaovirtual.IBalcaoVirtual.IProcessoNumeroPecaIdPdfGet;
 import br.jus.trf2.balcaovirtual.IBalcaoVirtual.ProcessoNumeroPecaIdPdfGetRequest;
@@ -18,6 +20,8 @@ public class ProcessoNumeroPecaIdPdfGet implements IProcessoNumeroPecaIdPdfGet {
 			origem = "pub";
 		} else {
 			Usuario u = BalcaoVirtualServlet.getPrincipal();
+			if (u == null)
+				throw new SwaggerAuthorizationException("Usuário não autenticado e token inexistente ou inválido");
 			usuario = u.usuario;
 			if (u.usuarios.get(req.sistema) != null)
 				origem = u.usuarios.get(req.sistema).origem;
